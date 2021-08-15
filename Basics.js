999a905aebe73272f799e3c913f4e09af449cc74
AimenYaseen <aimenyaseen6@gmail.com>
2021-08-15 08:04:53 +0500

    JavaScript Basics

--- Basics.js
+++ Basics.js
@@ -7,6 +7,8 @@ number = 78; // No Error
 console.log(number);
 
 // Constants
+const lastName = "Yaseen";
+console.log(lastName);
 const num = 50; // you can not change the value of constants
 num = 20; //ERROR
 console.log(num);
